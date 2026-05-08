3c046eed9e4b03c526096113e9d041de6b36e061
Robert Maidstone <robert.maidstone@manchester.ac.uk>
2026-05-08 13:51:05 +0100

    plotting code edits

--- plotting_functions.R
+++ plotting_functions.R
@@ -8,9 +8,8 @@ plot_OR <- function(file,p_val=TRUE){
     "Sometimes"="Irregular shift work\nincluding nights",
     "Always"="Permanent night shift\nwork"
   ))
-  cbPalette <- c("red","black","red")
+  cbPalette <- c("red","black")
   pd_width <- 0.6
-  y_lim <- c(.6,1.5)
   
   readxl::read_xlsx(file) -> plot_data
   
@@ -23,10 +22,13 @@ plot_OR <- function(file,p_val=TRUE){
     separate(values,
              into=c("OR","LCI","UCI"),
              sep="[()\\– ]+",
-             convert=TRUE) %>%
+             convert=TRUE,
+             extra="drop",
+             fill="right") %>%
     mutate(OR=ifelse(`Shift work`=="No shift work",1,OR)) %>%
     mutate(`Shift work`=factor(recode(`Shift work`,!!!shift_vars),levels=unname(shift_vars),ordered=TRUE))
   
+  y_lim <- c(0.975*min(clean_data$LCI,na.rm=T),max(clean_data$UCI,na.rm=T)*1.025)
   
   plots<-list()
   
@@ -35,24 +37,23 @@ plot_OR <- function(file,p_val=TRUE){
     p_mod<- clean_data %>% 
       filter(model==i) %>%
       ggplot(aes(y=OR,x=`Shift work`,colour=Sex)) + 
-      geom_hline(aes(yintercept = 1), size = .25, linetype = "dashed") +
+      geom_hline(aes(yintercept = 1), linewidth = .25, linetype = "dashed") +
       geom_errorbar(aes(ymax = UCI, ymin = LCI),
-                    size = .5, width = .4,
+                    linewidth = .5, width = .4,
                     position = position_dodge(width = pd_width)) +
       geom_point(position = position_dodge(width = pd_width)) + 
-      theme_bw() +
+      theme_classic() +
       theme(axis.title.y = element_blank(),
-            legend.position=c(0.85, 0.85),
+            legend.position=c(0.85, 0.925),
             legend.background = element_blank())+
-      ylab("Odds Ratio") +
+      ylab("Adjusted odds ratio\nof medicated asthma") +
       ylim(y_lim) +
       coord_flip() +
-      scale_x_discrete(limits = rev(levels(plot_data$`Shift work`)))+
       scale_colour_manual(
-        values = c(cbPalette[c(2,3)],"black"),
-        name = element_blank(),
+        values = cbPalette,
+        name = "",
         guide = guide_legend(reverse=TRUE)
-        )
+      )
     
     if (p_val) {
       pval <- plot_data %>%
@@ -60,8 +61,8 @@ plot_OR <- function(file,p_val=TRUE){
         pull(sex_interaction) %>%
         unique()
       
-      pval_text <- ifelse(pval < 0.001, "p<0.001", paste0("p=", pval))
-      y_pos <- (y_lim[1]+1)/2
+      pval_text <- ifelse(pval < 0.0001, "p<0.0001", paste0("p=", pval))
+      y_pos <- (1-y_lim[1])/4 +y_lim[1]
       x_pos <- length(unique(clean_data$`Shift work`)) +.25
       
       p_mod <- p_mod +
@@ -75,3 +76,66 @@ plot_OR <- function(file,p_val=TRUE){
   }
   return(plots)
 }
+
+
+
+plot_OR_UKB_CT <- function(file,p_val=TRUE){
+  
+  shift_vars <- rev(c(
+    "No shift work"= "Day workers (referent)",
+    "Never/rarely"="Shift work, but never or\nrarely night shifts",
+    "Sometimes"="Irregular shift work\nincluding nights",
+    "Always"="Permanent night shift\nwork"
+  ))
+  cbPalette <- c("red","black","blue")
+  pd_width <- 0.6
+  
+  readxl::read_xlsx(file) -> plot_data
+  
+  clean_data <- plot_data %>%
+    dplyr::select(-vars,-sex_interaction) %>%
+    pivot_longer(cols = -c(model,Sex),
+                 names_to="Shift work",
+                 values_to="values") %>%
+    separate(values,
+             into=c("OR","LCI","UCI"),
+             sep="[()\\–\\- ]+",
+             convert=TRUE,
+             extra="drop",
+             fill="right") %>%
+    mutate(OR=ifelse(`Shift work`=="No shift work",1,OR)) %>%
+    mutate(`Shift work`=factor(recode(`Shift work`,!!!shift_vars),levels=unname(shift_vars),ordered=TRUE))
+  
+  y_lim <- c(0.975*min(clean_data$LCI,na.rm=T),max(clean_data$UCI,na.rm=T)*1.025)
+  
+  plots<-list()
+  
+  for(i in unique(clean_data$Sex)){
+    
+    p_mod<- clean_data %>% 
+      filter(Sex==i) %>%
+      ggplot(aes(y=OR,x=`Shift work`)) + 
+      geom_hline(aes(yintercept = 1), linewidth = .25, linetype = "dashed") +
+      geom_errorbar(aes(ymax = UCI, ymin = LCI),
+                    linewidth = .5, width = .4,
+                    position = position_dodge(width = pd_width)) +
+      geom_point(position = position_dodge(width = pd_width)) + 
+      theme_classic() +
+      theme(axis.title.y = element_blank(),
+            legend.position=c(0.85, 0.925),
+            legend.background = element_blank())+
+      ylab("Adjusted odds ratio\nof moderate-severe asthma") +
+      ylim(y_lim) +
+      coord_flip() +
+      scale_colour_manual(
+        values = cbPalette,
+        name = "",
+        guide = guide_legend(reverse=TRUE)
+      )
+    
+    plots[[as.character(i)]] <- p_mod
+    
+  }
+  return(plots)
+}
+
--- plotting_v2.R
+++ plotting_v2.R
@@ -8,10 +8,18 @@ tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW.xlsx")
 
 tt[[1]] + tt[[2]] + tt[[3]]
 
+ggsave(plot = tt[[1]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_m1.png",width=5,height=4)
+ggsave(plot = tt[[2]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_m2.png",width=5,height=4)
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_m3.png",width=5,height=4)
 
-tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_age_q1.xlsx")
 
-tt[[1]] + tt[[2]] + tt[[3]]
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_CT_E.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_CT_E_m3.png",width=5,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_CT_I.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_CT_I_m3.png",width=5,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_CT_M.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_CT_M_m3.png",width=5,height=4)
+
 
 
 tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_manlabour_O.xlsx")
