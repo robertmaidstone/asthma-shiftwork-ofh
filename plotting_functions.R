48dbeb26d2266d504ae2b6511021ddd491b9c7b4
Robert Maidstone <robert.maidstone@manchester.ac.uk>
2026-05-12 18:32:33 +0100

    updating plotting

--- plotting_functions.R
+++ plotting_functions.R
@@ -6,7 +6,8 @@ plot_OR <- function(file,p_val=TRUE){
    "No shift work"= "Day workers (referent)",
     "Never/rarely"="Shift work, but never or\nrarely night shifts",
     "Sometimes"="Irregular shift work\nincluding nights",
-    "Always"="Permanent night shift\nwork"
+    "Always"="Permanent night shift\nwork",
+   "Shift work"="Shift workers"
   ))
   cbPalette <- c("red","black")
   pd_width <- 0.6
@@ -138,4 +139,82 @@ plot_OR_UKB_CT <- function(file,p_val=TRUE){
   }
   return(plots)
 }
+
+plot_ORb <- function(file,p_val=TRUE){
+  
+  shift_vars <- rev(c(
+    "No shift work"= "Day workers (referent)",
+    "Never/rarely"="Shift work, but never or\nrarely night shifts",
+    "Sometimes"="Irregular shift work\nincluding nights",
+    
+  ))
+  cbPalette <- c("red","black")
+  pd_width <- 0.6
+  
+  readxl::read_xlsx(file) -> plot_data
+  
+  clean_data <- plot_data %>%
+    filter(vars=="formatted") %>%
+    dplyr::select(-vars,-sex_interaction) %>%
+    pivot_longer(cols = -c(model,Sex),
+                 names_to="Shift work",
+                 values_to="values") %>%
+    separate(values,
+             into=c("OR","LCI","UCI"),
+             sep="[()\\– ]+",
+             convert=TRUE,
+             extra="drop",
+             fill="right") %>%
+    mutate(OR=ifelse(`Shift work`=="No shift work",1,OR)) %>%
+    mutate(`Shift work`=factor(recode(`Shift work`,!!!shift_vars),levels=unname(shift_vars),ordered=TRUE))
+  
+  y_lim <- c(0.975*min(clean_data$LCI,na.rm=T),max(clean_data$UCI,na.rm=T)*1.025)
+  
+  plots<-list()
+  
+  for(i in unique(clean_data$model)){
+    
+    p_mod<- clean_data %>% 
+      filter(model==i) %>%
+      ggplot(aes(y=OR,x=`Shift work`,colour=Sex)) + 
+      geom_hline(aes(yintercept = 1), linewidth = .25, linetype = "dashed") +
+      geom_errorbar(aes(ymax = UCI, ymin = LCI),
+                    linewidth = .5, width = .4,
+                    position = position_dodge(width = pd_width)) +
+      geom_point(position = position_dodge(width = pd_width)) + 
+      theme_classic() +
+      theme(axis.title.y = element_blank(),
+            legend.position=c(0.85, 0.925),
+            legend.background = element_blank())+
+      ylab("Adjusted odds ratio\nof medicated asthma") +
+      ylim(y_lim) +
+      coord_flip() +
+      scale_colour_manual(
+        values = cbPalette,
+        name = "",
+        guide = guide_legend(reverse=TRUE)
+      )
+    
+    if (p_val) {
+      pval <- plot_data %>%
+        filter(model == i, !is.na(sex_interaction)) %>%
+        pull(sex_interaction) %>%
+        unique()
+      
+      pval_text <- ifelse(pval < 0.0001, "p<0.0001", paste0("p=", pval))
+      y_pos <- (1-y_lim[1])/4 +y_lim[1]
+      x_pos <- length(unique(clean_data$`Shift work`)) +.25
+      
+      p_mod <- p_mod +
+        annotate("text",
+                 x = x_pos,
+                 y = y_pos,
+                 label = pval_text) }
+    
+    plots[[as.character(i)]] <- p_mod
+    
+  }
+  return(plots)
+}
+
 
--- plotting_v2.R
+++ plotting_v2.R
@@ -13,6 +13,33 @@ ggsave(plot = tt[[2]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_m2.pn
 ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_m3.png",width=5,height=4)
 
 
+# model 2 vars ------------------------------------------------------------
+
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_model2vars.xlsx")
+
+p_c <- (tt[[1]]+ ggtitle("Model 1")+theme(axis.title.x = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank()) + tt[[2]] + theme(axis.title.x = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.text.y = element_blank(),axis.ticks.y=element_blank()) + ggtitle("Model 1 + alcohol status") + tt[[3]] + theme(axis.text.y = element_blank(),axis.ticks.y =element_blank(),axis.title.x = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank())+ ggtitle("Model 1 + days exercised"))/ 
+  (tt[[4]]+ ggtitle("Model 1 + length of working week")  + tt[[5]] + theme(axis.text.y = element_blank(),axis.ticks=element_blank()) + ggtitle("Model 1 + chronotype") + tt[[6]] + theme(axis.text.y = element_blank(),axis.ticks=element_blank())+ ggtitle("Model 1 + household income"))
+ggsave(plot = p_c, filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_model2vars.png",width=12,height=7)
+
+
+# model 3 vars ------------------------------------------------------------
+
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_model3vars.xlsx")
+
+p_c <- (tt[[1]]+ ggtitle("Model 2")+theme(axis.title.x = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank()) + tt[[2]] + theme(axis.title.x = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.text.y = element_blank(),axis.ticks.y=element_blank()) + ggtitle("Model 2 + BMI") + tt[[3]] + theme(axis.text.y = element_blank(),axis.ticks.y =element_blank(),axis.title.x = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank())+ ggtitle("Model 2 + smoking variables"))/ 
+  (tt[[4]]+ ggtitle("Model 2 + sleep duration")  + tt[[5]] + theme(axis.text.y = element_blank(),axis.ticks=element_blank()) + ggtitle("Model 2 + vaping")+plot_spacer())
+ggsave(plot = p_c, filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_model3vars.png",width=12,height=7)
+
+
+
+
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_manlabour_O.xlsx")
+
+tt[[1]] + tt[[2]] + tt[[3]]
+
+
+# Chronotype --------------------------------------------------------------
+
 tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_CT_E.xlsx")
 ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_CT_E_m3.png",width=5,height=4)
 tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_CT_I.xlsx")
@@ -20,8 +47,53 @@ ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_CT_I_
 tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_CT_M.xlsx")
 ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_CT_M_m3.png",width=5,height=4)
 
+tt <- plot_OR_UKB_CT("~/OurFutureHealth/OFHresults/UKB_CS_SW_CT_E.xlsx")
+ggsave(plot = tt$Morning, filename="OurFutureHealth/ofh-shiftwork/plots/UKB_CS_SW_CT_M.png",width=5,height=4)
+ggsave(plot = tt$Intermediate, filename="OurFutureHealth/ofh-shiftwork/plots/UKB_CS_SW_CT_I.png",width=5,height=4)
+ggsave(plot = tt$Evening, filename="OurFutureHealth/ofh-shiftwork/plots/UKB_CS_SW_CT_E.png",width=5,height=4)
+
+# Age --------------------------------------------------------------
+
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_age_q1.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_age_1_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_age_q2.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_age_2_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_age_q3.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_age_3_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_age_q4.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_age_4_m3.png",width=3.75,height=4)
 
 
-tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_manlabour_O.xlsx")
+# Ethnicity --------------------------------------------------------------
 
-tt[[1]] + tt[[2]] + tt[[3]]
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_eth_A.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_eth_A_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_eth_Black.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_eth_Black_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_eth_Chinese.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_eth_Chinese_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_eth_Mixed.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_eth_Mixed_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_eth_Other.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_eth_Other_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_eth_WB.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_eth_WB_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SW_eth_WO.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SW_eth_WO_m3.png",width=3.75,height=4)
+
+# Ethnicity =- binary shift work --------------------------------------------------------------
+
+tt <- plot_ORb("~/OurFutureHealth/OFHresults/CS_SWb_eth_A.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SWb_eth_A_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SWb_eth_Black.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SWb_eth_Black_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SWb_eth_Chinese.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SWb_eth_Chinese_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SWb_eth_Mixed.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SWb_eth_Mixed_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SWb_eth_Other.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SWb_eth_Other_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SWb_eth_WB.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SWb_eth_WB_m3.png",width=3.75,height=4)
+tt <- plot_OR("~/OurFutureHealth/OFHresults/CS_SWb_eth_WO.xlsx")
+ggsave(plot = tt[[3]], filename="OurFutureHealth/ofh-shiftwork/plots/CS_SWb_eth_WO_m3.png",width=3.75,height=4)
